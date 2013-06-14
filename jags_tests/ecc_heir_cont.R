c505d9e7e902523b5facc6337539bff1fb43da02
Eric Ford <eford>
2013-06-13 20:10:42 -0400

    Updated data files and scripts, so common column names

--- jags_tests/ecc_heir_cont.R
+++ jags_tests/ecc_heir_cont.R
@@ -1,41 +1,66 @@
-#dcat
-#dmmult
-#ddirc
 library(R2jags)
 load.module("glm") #improves performance
 
-inputdata <- read.table("darin_hkeasy.txt", header=T)
+#inputdata <- read.table("darin_hkeasy.txt", header=T)
+#inputdata <- read.table("darin_exopops.txt", header=T)
+inputdata <- read.table("tom_hk.txt", header=T)
 #inputdata
-Ndata <- length(inputdata$HHAT)
+Ndata <- length(inputdata$H_OBS)
 
 ecc_mixture_cont <- function() {
 alpha <- 2.0
 sigmae ~ dunif(0.,1.)
 for (i in 1:Ndata) { 
  lambda[i] ~ dgamma(alpha/2,alpha/2)
- h[i] ~ dnorm(0.,lambda[i]/(sigmae*sigmae)) %_% T(-1,1) 
- k[i] ~ dnorm(0.,lambda[i]/(sigmae*sigmae)) %_% T(-1,1)
+ h[i] ~ dnorm(0.0,lambda[i]/(sigmae*sigmae)) %_% T(-1,1) 
+ k[i] ~ dnorm(0.0,lambda[i]/(sigmae*sigmae)) %_% T(-1,1)
  hhat[i] ~ dnorm(h[i],1.0/(sigmahobs[i]*sigmahobs[i])) %_% T(-1,1)
  khat[i] ~ dnorm(k[i],1.0/(sigmakobs[i]*sigmakobs[i])) %_% T(-1,1)
  }
 }
-inits = NULL  # initial values
-
 model.file = "ecc_heir_cont.txt"
 write.model(ecc_mixture_cont, model.file)
-file.show(model.file)
+#file.show(model.file)
 
-data = list(hhat=inputdata$HHAT,khat=inputdata$KHAT,sigmahobs=inputdata$SIGMAH,sigmakobs=inputdata$SIGMAK,Ndata=Ndata)
+inits = NULL  # initial values
+
+data = list(hhat=inputdata$H_OBS,khat=inputdata$K_OBS,sigmahobs=inputdata$H_SIGMA,sigmakobs=inputdata$K_SIGMA,Ndata=Ndata)
 parameters.to.save = c("h", "k", "sigmae","lambda")
 
-sim = jags(data, inits, parameters.to.save, model.file=model.file, n.chains=2, n.iter=500)
+sim = jags(data, inits, parameters.to.save, model.file=model.file, n.chains=2, n.iter=100)
 
 #print(sim)
 #plot(sim)
 
-hist(inputdata$HTRUE)
-colnames(sims$BUGSoutput$sims.matrix)
+colnames(sim$BUGSoutput$sims.matrix)
 hist(sim$BUGSoutput$sims.matrix[,"h[1]"], prob=T, breaks=20)
 hfirstcol = 2
-hlastcol = hfirstcol + length(inputdata$HHAT)-1
+hlastcol = hfirstcol + length(inputdata$H_OBS)-1
+hpred <- sim$BUGSoutput$sims.matrix[,hfirstcol:hlastcol]
+hist(hpred, prob=T, breaks=20)
+hist(inputdata$H_TRUE, breaks=20)
+kfirstcol = hlastcol + 1
+klastcol = kfirstcol + length(inputdata$K_OBS)-1
+kpred <- sim$BUGSoutput$sims.matrix[,kfirstcol:klastcol]
+hist(kpred, prob=T, breaks=20)
+hist(inputdata$K_TRUE, breaks=20)
+
+epred <- sqrt(hpred^2+kpred^2)
+hist(epred, prob=T, breaks=20)
+hist(sqrt(inputdata$H_TRUE^2+inputdata$K_TRUE^2), breaks=20)
+
+sigmae_post <- sim$BUGSoutput$sims.matrix[,"sigmae"]
+hist(sigmae_post, prob=T, breaks=20)
+
+
+
+
+
+hist(inputdata$H_TRUE, prob=T, breaks=20)
+colnames(sim$BUGSoutput$sims.matrix)
+#hist(sim$BUGSoutput$sims.matrix[,"h[1]"], prob=T, breaks=20)
+hfirstcol = 2
+hlastcol = hfirstcol + length(inputdata$H_OBS)-1
 hist(sim$BUGSoutput$sims.matrix[,hfirstcol:hlastcol], prob=T, breaks=20)
+
+hist(sim$BUGSoutput$sims.matrix[,"sigmae"], prob=T, breaks=20)
--- jags_tests/ecc_heir_disc.R
+++ jags_tests/ecc_heir_disc.R
@@ -1,48 +1,72 @@
-#dcat
-#dmmult
-#ddirc
 library(R2jags)
 load.module("glm") #improves performance
 
-#inputdata <- read.table("ecc_data.txt", header=T)
-#inputdata <- read.table("https://raw.github.com/exosamsi/EccentricityDistribution/master/datasets/darin_hkeasy.txt", header=T)
 inputdata <- read.table("darin_hkeasy.txt", header=T)
+#inputdata <- read.table("darin_exopops.txt", header=T)
+#inputdata <- read.table("tom_hk.txt", header=T)
 #inputdata
-Ndata <- length(inputdata$HHAT)
+Ndata <- length(inputdata$H_OBS)
 
-ecc_mixture_disc <- function() {
-#sigmahobs <- 0.01
-#sigmakobs <- 0.01
-#alpha <- 2.0
-#f ~ ddirchilet(3)
+ecc_disc_rayleigh <- function() {
 sigmae ~ dunif(0.,1.)
-#Ndata <- 905
-#for (i in 1:5) { 
 for (i in 1:Ndata) { 
- #lambda[i] ~ dgamma(alpha/2,alpha/2)
  h[i] ~ dnorm(0.,1.0/(sigmae*sigmae)) %_% T(-1,1) # *lambda[i]
  k[i] ~ dnorm(0.,1.0/(sigmae*sigmae)) %_% T(-1,1)
  hhat[i] ~ dnorm(h[i],1.0/(sigmahobs[i]*sigmahobs[i])) %_% T(-1,1)
  khat[i] ~ dnorm(k[i],1.0/(sigmakobs[i]*sigmakobs[i])) %_% T(-1,1)
  }
 }
+model.file = "ecc_heir_rayleigh.txt"
+write.model(ecc_disc_rayleigh, model.file)
+#file.show(model.file)
+
+Ncomp <- 3
+# doesn't work yet
+ecc_mixture_disc_three <- function() {
+for (cc in 1:Ncomp) { 
+ sigmae[cc] ~ dunif(0.,1.)
+}
+f[1:3] ~ ddirch(3) #don't work yet
+for (i in 1:Ndata) {  
+ c[i] ~ dcat(f) #don't work yet
+ h[i] ~ dnorm(0.,1.0/(sigmae[c]*sigmae[c])) %_% T(-1,1) 
+ k[i] ~ dnorm(0.,1.0/(sigmae[c]*sigmae[c])) %_% T(-1,1)
+ hhat[i] ~ dnorm(h[i],1.0/(sigmahobs[i]*sigmahobs[i])) %_% T(-1,1)
+ khat[i] ~ dnorm(k[i],1.0/(sigmakobs[i]*sigmakobs[i])) %_% T(-1,1)
+ }
+}
+model.file = "ecc_heir_disc.txt"
+write.model(ecc_mixture_disc_three, model.file)
+#file.show(model.file)
+
 inits = NULL  # initial values
 
-model.file = "ecc_heir_disc.txt"
-write.model(ecc_mixture_disc, model.file)
-file.show(model.file)
-
-data = list(hhat=inputdata$HHAT,khat=inputdata$KHAT,sigmahobs=inputdata$SIGMAH,sigmakobs=inputdata$SIGMAK,Ndata=Ndata)
+data = list(hhat=inputdata$H_OBS,khat=inputdata$K_OBS,sigmahobs=inputdata$H_SIGMA,sigmakobs=inputdata$K_SIGMA,Ndata=Ndata)
+#data = list(hhat=inputdata$H_OBS,khat=inputdata$K_OBS,sigmahobs=inputdata$H_SIGMA,sigmakobs=inputdata$K_SIGMA,Ndata=Ndata,Ncomp=Ncomp)
 parameters.to.save = c("h", "k", "sigmae")
 
-sim = jags(data, inits, parameters.to.save, model.file=model.file, n.chains=2, n.iter=5000)
+sim = jags(data, inits, parameters.to.save, model.file=model.file, n.chains=2, n.iter=100)
 
 #print(sim)
 #plot(sim)
 
-hist(inputdata$HTRUE)
-colnames(sims$BUGSoutput$sims.matrix)
+colnames(sim$BUGSoutput$sims.matrix)
 hist(sim$BUGSoutput$sims.matrix[,"h[1]"], prob=T, breaks=20)
 hfirstcol = 2
-hlastcol = hfirstcol + length(inputdata$HHAT)-1
-hist(sim$BUGSoutput$sims.matrix[,hfirstcol:hlastcol], prob=T, breaks=20)
+hlastcol = hfirstcol + length(inputdata$H_OBS)-1
+hpred <- sim$BUGSoutput$sims.matrix[,hfirstcol:hlastcol]
+hist(hpred, prob=T, breaks=20)
+hist(inputdata$H_TRUE, breaks=20)
+kfirstcol = hlastcol + 1
+klastcol = kfirstcol + length(inputdata$K_OBS)-1
+kpred <- sim$BUGSoutput$sims.matrix[,kfirstcol:klastcol]
+hist(kpred, prob=T, breaks=20)
+hist(inputdata$K_TRUE, breaks=20)
+
+epred <- sqrt(hpred^2+kpred^2)
+hist(epred, prob=T, breaks=20)
+hist(sqrt(inputdata$H_TRUE^2+inputdata$K_TRUE^2), breaks=20)
+
+sigmae_post <- sim$BUGSoutput$sims.matrix[,"sigmae"]
+hist(sigmae_post, prob=T, breaks=20)
+
